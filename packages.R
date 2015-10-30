5d5f117363ba01fcb85b815d981e62fc5fda2f80
Brian Danielak <brian@briandk.com>
2015-10-30 14:26:02 -0400

    Modularize R package requirements

---
+++ packages.R
@@ -0,0 +1,14 @@
+packages_to_install <- c(
+  "checkpoint",
+  "dplyr",
+  "ggplot2",
+  "knitr",
+  "magrittr",
+  "rmarkdown",
+)
+
+install.packages(
+  packages_to_install,
+  dep = TRUE,
+  repos = "http://cran.rstudio.com"
+)
